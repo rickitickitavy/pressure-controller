8739f61d27812bbab97c9f47b34389f5c14e8aca
rickitickitav@gmail.com <rickitickitav@gmail.com>
2024-04-20 23:16:59 +0300

    correction of the max and the min pressure

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,9 @@ float lastDisplayedValue = 0;
 float emaValue = -1;
 float ema = 0.3;
 
-Adafruit_ST7789* tft;
+Adafruit_ST7789 *tft;
 
-enum BUTTON_CODE{
+enum BUTTON_CODE {
     NONE = 0, MAX_UP, MAX_DOWN, MIN_MAX, MIN_DOWN, LOCK
 };
 
@@ -34,7 +34,7 @@ float min_pressure = 1.7;
 
 #define ST77XX_DARKGREEN 0x03E0
 
-void setup(){
+void setup() {
     Serial.begin(57600);
     delay(250);
     Serial.println("sssppp5");
@@ -54,27 +54,31 @@ void setup(){
     tft->setFont(&FreeMonoBoldOblique24pt7b);
     tft->fillRect(0, 0, 240, 64, ST77XX_RED);
     tft->setCursor(7, 42);
-    tft->printf("Max %s", String(max_pressure).c_str());
+    tft->print("Max");
+    tft->setCursor(112, 42);
+    tft->print(String(max_pressure));
 
     tft->fillRect(0, 220, 240, 64, ST77XX_DARKGREEN);
     tft->setCursor(7, 262);
     tft->setTextColor(ST77XX_WHITE);
-    tft->printf("Min %s", String(min_pressure).c_str());
+    tft->print("Min");
+    tft->setCursor(112, 262);
+    tft->print(String(min_pressure));
 
     pinMode(A0, INPUT);
     for (int index = 0; index < 5; pinMode(button_pins[index++], INPUT_PULLUP));
 }
 
-bool readStatusButton(int index){
+bool readStatusButton(int index) {
     long time = millis();
-    if ((time - button_press_time[index]) > anti_buzzle_ms){
+    if ((time - button_press_time[index]) > anti_buzzle_ms) {
         // it's time to check button state
         int free = digitalRead(button_pins[index]);
-        if (!button_pressed_status[index] && (!free)){
+        if (!button_pressed_status[index] && (!free)) {
             button_pressed_status[index] = true;
             button_press_time[index] = time;
             return true;
-        } else if ((button_pressed_status[index]) && (free)){
+        } else if ((button_pressed_status[index]) && (free)) {
             button_pressed_status[index] = false;
             button_press_time[index] = time;
             return false;
@@ -84,21 +88,77 @@ bool readStatusButton(int index){
 
 }
 
-int testButtons(){
-    for (int index =0; index < 5; index++){
+int testButtons() {
+    for (int index = 0; index < 5; index++) {
         if (readStatusButton(index))
             return index;
     }
     return -1;
 }
 
-void manageButtons(){
+void manageButtons() {
+    bool max_changed = false;
+    bool min_changed = false;
+
     int button_code = testButtons();
-//    if (button_code == 0)
+    switch (button_code) {
+        case 0:
+            if (max_pressure < 3.8F) {
+                max_pressure += 0.05;
+                max_changed = true;
+            }
+            break;
+        case 1:
+            if (max_pressure > 2.001F) {
+                max_pressure -= 0.05;
+                max_changed = true;
+
+                if (min_pressure > (max_pressure - 0.5)) {
+                    min_pressure = max_pressure - 0.5;
+                    min_changed = true;
+                }
+            }
+            break;
+        case 2:
+            if (min_pressure < 2.2F) {
+                min_pressure += 0.05;
+                min_changed = true;
+
+                if (min_pressure > (max_pressure - 0.5)) {
+                    max_pressure = min_pressure + 0.5;
+                    max_changed = true;
+                }
+            }
+            break;
+        case 3:
+            if (min_pressure > 1.201F) {
+                min_pressure -= 0.05;
+                min_changed = true;
+            }
+            break;
+        default:
+            break;
+    }
+
+    if (max_changed) {
+        tft->setTextColor(ST77XX_YELLOW);
+        tft->fillRect(112, 8, 120, 38, ST77XX_RED);
+        tft->setCursor(112, 42);
+        tft->print(String(max_pressure));
+        Serial.println("max + " + String(max_pressure));
+    }
+
+    if (min_changed) {
+        tft->setTextColor(ST77XX_WHITE);
+        tft->fillRect(112, 228, 120, 38, ST77XX_DARKGREEN);
+        tft->setCursor(112, 262);
+        tft->print(String(min_pressure));
+    }
 
 }
 
-void loop(){
+void loop() {
+    manageButtons();
     if ((millis() - lastWork) > 20) {
         lastWork = millis();
         float val = analogRead(A0) * 25.0F / (1024.0F * 3.3F);
@@ -108,7 +168,7 @@ void loop(){
             emaValue = emaValue * (1.0F - ema) + val * ema;
 
         if ((abs(lastDisplayedValue - val) > 0.05)
-            || ((abs(lastDisplayedValue - emaValue) >= 0.01) && ((lastWork - lastScreenUpdated) > 500))){
+            || ((abs(lastDisplayedValue - emaValue) >= 0.01) && ((lastWork - lastScreenUpdated) > 500))) {
             lastDisplayedValue = emaValue;
             lastScreenUpdated = lastWork;
 
