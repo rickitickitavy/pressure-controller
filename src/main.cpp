fcd959f5fff5848cef0e7b303aa12ad867403a26
rickitickitav@gmail.com <rickitickitav@gmail.com>
2024-05-08 19:26:56 +0300

    added rotary encoder

--- src/main.cpp
+++ src/main.cpp
@@ -28,21 +28,25 @@ struct settings_t{
     long time_to_reach_half_of_pressure = 40000;
     long max_pump_on_time = 120000;
     long time_to_reach_min_pressure = 5000;
+    long time_press_for_on_off_pump_ms = 1000;
     float sensor_V = 5.0F;
     long scan_sensor_sensor_ms = 20;
     float sensor_corr = 0.917;
 };
 
-long button_press_time[5] = {0, 0, 0, 0, 0};
-bool button_pressed_status[5] = {false, false, false, false, false};
-int button_pins[5] = {7, 6, 5, 4, 3};
-long anti_buzzle_ms = 50;
+long button_press_time = 0;
+bool button_pressed_status = false;
+long anti_buzzle_ms = 150;
 settings_t settings;
 bool pump_enabled = true;
 bool pump_active = false;
 long pump_started_at = 0;
 bool failed = false;
 long min_max_changed_at = -1;
+
+int action_code = -1;
+int range_index = -1;
+bool long_press = false;
 
 float v_op;
 
@@ -101,6 +105,57 @@ void drawMaxPressure(){
     tft->print(String(settings.max_pressure));
 }
 
+void rotaryDetected(){
+    if (range_index >= 0) {
+        if (digitalRead(4))
+            action_code = 1 + (range_index << 1);
+        else
+            action_code = 0 + (range_index << 1);
+    }
+}
+
+int readButton() {
+    long time = millis();
+    if ((time - button_press_time) > anti_buzzle_ms) {
+        // it's time to check button state
+        int free = digitalRead(3);
+
+        if (!free && long_press)
+            return -1;
+
+        long_press = false;
+
+        if (!button_pressed_status && (!free)) {
+            button_pressed_status = true;
+            button_press_time = time;
+            return 5;
+        } else if ((button_pressed_status) && (free)) {
+            button_pressed_status = false;
+            button_press_time = time;
+            return -1;
+        } else if (button_pressed_status && !free && ((time - button_press_time) > settings.time_press_for_on_off_pump_ms)) {
+            button_press_time = time;
+            long_press = true;
+            return 4;
+        }else
+            return -1;
+    } else
+        return -1;
+
+}
+
+int getCurrentAction() {
+    int btn_code = readButton();
+    if (btn_code != -1)
+        return btn_code;
+    else {
+        int ret_value = action_code;
+        action_code = -1;
+        return ret_value;
+    }
+}
+
+
 void setup() {
     pinMode(PUMP_PIN, OUTPUT);
     digitalWrite(PUMP_PIN, LOW);
@@ -133,38 +188,26 @@ void setup() {
     drawMinPressure();
 
     pinMode(A0, INPUT);
-    for (int index = 0; index < 5; pinMode(button_pins[index++], INPUT_PULLUP));
 
     drawPump(ST77XX_DARKGRAY2);
 
     v_op = 5.0F / settings.sensor_V;
-}
 
-bool readStatusButton(int index) {
-    long time = millis();
-    if ((time - button_press_time[index]) > anti_buzzle_ms) {
-        // it's time to check button state
-        int free = digitalRead(button_pins[index]);
-        if (!button_pressed_status[index] && (!free)) {
-            button_pressed_status[index] = true;
-            button_press_time[index] = time;
-            return true;
-        } else if ((button_pressed_status[index]) && (free)) {
-            button_pressed_status[index] = false;
-            button_press_time[index] = time;
-            return false;
-        } else
-            return false;
-    }
+    pinMode(2, INPUT);
+    pinMode(3, INPUT_PULLUP);
+    pinMode(4, INPUT);
+
+    attachInterrupt(digitalPinToInterrupt(2), rotaryDetected, FALLING);
 
 }
 
-int testButtons() {
-    for (int index = 0; index < 5; index++) {
-        if (readStatusButton(index))
-            return index;
+void drawOffEditMode(){
+    switch (range_index) {
+        case 0: tft->fillRect(0, 53, 240, 6, ST77XX_RED);
+            break;
+        case 1: tft->fillRect(0, 293, 240, 6, ST77XX_DARKGREEN);
+            break;
     }
-    return -1;
 }
 
 void manageButtons() {
@@ -176,6 +219,8 @@ void manageButtons() {
             min_max_changed_at = -1;
             saveSettings();
             tft->fillCircle(30, 95, 10, ST77XX_BLACK);
+            drawOffEditMode();
+            range_index = -1;
         }
     }
 
@@ -183,48 +228,63 @@ void manageButtons() {
     bool min_changed = false;
     bool pump_enabled_changed = false;
 
-    int button_code = testButtons();
+    int button_code = getCurrentAction();//testButtons();
     switch (button_code) {
         case 0:
             if (settings.max_pressure < 3.8F) {
-                settings.max_pressure += 0.05;
+                settings.max_pressure += 0.01;
                 max_changed = true;
             }
             break;
         case 1:
             if (settings.max_pressure > 2.001F) {
-                settings.max_pressure -= 0.05;
+                settings.max_pressure -= 0.01;
                 max_changed = true;
 
-                if (settings.min_pressure > (settings.max_pressure - 0.5)) {
-                    settings.min_pressure = settings.max_pressure - 0.5;
+                if (settings.min_pressure > (settings.max_pressure - 0.1)) {
+                    settings.min_pressure = settings.max_pressure - 0.1;
                     min_changed = true;
                 }
             }
             break;
         case 2:
             if (settings.min_pressure < 2.2F) {
-                settings.min_pressure += 0.05;
+                settings.min_pressure += 0.01;
                 min_changed = true;
 
-                if (settings.min_pressure > (settings.max_pressure - 0.5)) {
-                    settings.max_pressure = settings.min_pressure + 0.5;
+                if (settings.min_pressure > (settings.max_pressure - 0.1)) {
+                    settings.max_pressure = settings.min_pressure + 0.1;
                     max_changed = true;
                 }
             }
             break;
         case 3:
             if (settings.min_pressure > 1.201F) {
-                settings.min_pressure -= 0.05;
+                settings.min_pressure -= 0.01;
                 min_changed = true;
             }
             break;
         case 4:
+            drawOffEditMode();
+            range_index = -1;
             pump_enabled = !pump_enabled;
             pump_enabled_changed = true;
             if (!pump_enabled) {
                 pump_active = false;
                 digitalWrite(PUMP_PIN, LOW);
+            }
+            break;
+        case 5:
+            drawOffEditMode();
+            range_index++;
+            if (range_index > 1)
+                range_index = -1;
+
+            switch (range_index) {
+                case 0: tft->fillRect(0, 53, 240, 6, ST77XX_YELLOW);
+                    break;
+                case 1: tft->fillRect(0, 293, 240, 6, ST77XX_WHITE);
+                    break;
             }
             break;
         default:
@@ -288,7 +348,7 @@ void loop() {
     }
     if ((millis() - lastWork) > settings.scan_sensor_sensor_ms) {
         lastWork = millis();
-        real_value = analogRead(A0) * v_op * (ADC_V / ADC_STEPS) * settings.sensor_corr;
+        real_value = analogRead(A0) * v_op * (ADC_V / ADC_STEPS) * settings.sensor_corr - 1.0F;
         if (ema_value == -1)
             ema_value = real_value;
         else
